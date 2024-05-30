f67b48afb7a24209b35c274f45bdf030b718c71a
Uzuko <38263@mail.ru>
2024-05-30 20:58:16 +0300

    initial commit

--- project_news/portal_news/NewsPaper/NewsPaper/settings.py
+++ project_news/portal_news/NewsPaper/NewsPaper/settings.py
@@ -180,3 +180,11 @@ ADMINS = (
 
 
 APSCHEDULER_DATETIME_FORMAT ='N j, Y, f:s a'
+
+
+# Данные для Redis:
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
