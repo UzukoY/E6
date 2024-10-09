90b1cf62ec62af9fdae7ed190641419ab03af107
Uzuko <38263@mail.ru>
2024-10-09 20:56:47 +0300

    initial

---
+++ Е6_final/messenger/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Chat, Message
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class MessageSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'chat', 'user', 'text', 'timestamp']
+
+class ChatSerializer(serializers.ModelSerializer):
+    users = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
+
+    class Meta:
+        model = Chat
+        fields = ['id', 'name', 'users']
---
+++ Е6_final/messenger/api/views.py
@@ -0,0 +1,29 @@
+from rest_framework import viewsets, permissions
+from .models import Chat, Message
+from .serializers import ChatSerializer, MessageSerializer, UserSerializer
+from django.contrib.auth.models import User
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ChatViewSet(viewsets.ModelViewSet):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        chat = serializer.save()
+        chat.users.add(self.request.user)
+
+    def get_queryset(self):
+        # Показывать только чаты, в которых участвует текущий пользователь
+        return Chat.objects.filter(users=self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
